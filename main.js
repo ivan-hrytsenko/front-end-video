0b747a8306fa59c27b54ec762e29501543537812
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2026-06-06 04:13:57 +0300

    add background color change

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import { showDeveloperInfo } from './modules/developer.js';
 
 showDeveloperInfo("Гриценко", "Іван");
+
+document.body.style.backgroundColor = "#fff0db";
+setTimeout(() => {
+    document.body.style.backgroundColor = "#ffffff";
+}, 30000);
